a3ef2c78e6534c6248bc29dd3efafdf052de309f
CCDM12 <ccdm12@CCDM12.local>
2018-11-01 10:03:36 -0700

    script Java

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,6 +1,26 @@
-var colorAmarillo = function(x){
-	x.style.background = "yellow"
-}
-var colorBlanco = function(x){
-	x.style.background = "white"
+window.onload = function(){
+	var colorAmarillo = function (){
+		this.style.background = "yellow";
+	}
+	var colorBlanco = function (){
+		this.style.background = "white";
+	}
+	var operando1 = document.getElementById("operando1");
+	operando1.addEventListener("focus",colorAmarillo);
+	operando1.addEventListener("focusout",colorBlanco);
+	
+	var operador ="";
+	var numeros = function(){
+		var valor = this.value;
+		if (operador=="") {/*operando1*/
+			var valorInput=document.getElementById("operando1").value;
+			if (valor=="0") {
+				document.getElementById("operando1").value="";
+			}
+			document.getElementById("operando1").value+=valor;
+		}else /*operando2* /*/
+	}
+
+	var uno = document.getElementById("uno")
+	uno.addEventListener("click", numeros)
 }

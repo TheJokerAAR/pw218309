ef02f664a286d4b52517ef49b54b0c88fde5a50b
CCDM12 <ccdm12@CCDM12.local>
2018-11-06 09:59:41 -0700

    Calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -5,22 +5,87 @@ window.onload = function(){
 	var colorBlanco = function (){
 		this.style.background = "white";
 	}
-	var operando1 = document.getElementById("operando1");
-	operando1.addEventListener("focus",colorAmarillo);
-	operando1.addEventListener("focusout",colorBlanco);
-	
+
 	var operador ="";
 	var numeros = function(){
 		var valor = this.value;
+		var operando1 = document.getElementById("operando1");
+		var operando2 = document.getElementById("operando2");
 		if (operador=="") {/*operando1*/
 			var valorInput=document.getElementById("operando1").value;
 			if (valor=="0") {
 				document.getElementById("operando1").value="";
 			}
 			document.getElementById("operando1").value+=valor;
-		}else /*operando2* /*/
+		}else {/*operando2* /*/
+			if (valor=="0") {
+				document.getElementById("operando2").value="";
+			}
+			document.getElementById("operando2").value+=valor;
+		}
+
+	var operadores = function(){
+		operador = this.value
+	}
+	var resultadoIgual = function(){
+		oper1 = document.getElementById('operando1').value
+		oper2 = document.getElementById('operando2').value
+		document.getElementById('resultado').value=eval(oper1+operador+oper2)
+	}
+	var reinicio = function(){
+		document.getElementById('operando1').value="0"
+		document.getElementById('operando2').value="0"
+		document.getElementById('resultado').value="0"
+		operador = ""
 	}
 
+
+
+	var operando1 = document.getElementById("operando1");
+	var operando2 = document.getElementById("operando2");
+	var resultado = document.getElementById("resultado");
 	var uno = document.getElementById("uno")
+	var dos = document.getElementById("dos")
+	var tres = document.getElementById("tres")
+	var cuatro = document.getElementById("cuatro")
+	var cinco = document.getElementById("cinco")
+	var seis = document.getElementById("seis")
+	var siete = document.getElementById("siete")
+	var ocho = document.getElementById("ocho")
+	var nueve = document.getElementById("nueve")
+	var cero = document.getElementById("cero")
+	var suma = document.getElementById("suma")
+	var resta = document.getElementById("resta")
+	var multiplicacion = document.getElementById("multiplicacion")
+	var division = document.getElementById("division")
+	var igual = document.getElementById("igual")
+	var reiniciar = document.getElementById("reiniciar")
+	
+	operando1.addEventListener("focus",colorAmarillo);
+	operando1.addEventListener("focusout",colorBlanco);
+	operando2.addEventListener("focus",colorAmarillo);
+	operando2.addEventListener("focusout",colorBlanco);
+	resultado.addEventListener("focus",colorAmarillo);
+	resultado.addEventListener("focusout",colorBlanco);
+	reiniciar.addEventListener("click",reinicio);
+	
+	cero.addEventListener("click", numeros)
 	uno.addEventListener("click", numeros)
+	dos.addEventListener("click", numeros)
+	tres.addEventListener("click", numeros)
+	cuatro.addEventListener("click", numeros)
+	cinco.addEventListener("click", numeros)
+	seis.addEventListener("click", numeros)
+	siete.addEventListener("click", numeros)
+	ocho.addEventListener("click", numeros)
+	nueve.addEventListener("click", numeros)
+
+	suma.addEventListener("click", operadores)
+	resta.addEventListener("click", operadores)
+	multiplicacion.addEventListener("click", operadores)
+	division.addEventListener("click", operadores)
+
+	igual.addEventListener("click", resultadoIgual)
+
+
 }

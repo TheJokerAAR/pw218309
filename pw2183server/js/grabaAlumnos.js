8dd7e19e5168191b2fd6955adae065f6805fe1d7
CCDM12 <ccdm12@CCDM12.local>
2018-11-30 10:07:52 -0700

    trabajos

---
+++ pw2183server/js/grabaAlumnos.js
@@ -0,0 +1,6 @@
+var grabar=function(){
+	var nc=document.getElementById('ncontrol').value;
+	var no=document.getElementById('nombre').value;
+	var ap=document.getElementById('apellido').value;
+	var ed=document.getElementById('edad').value;
+}
